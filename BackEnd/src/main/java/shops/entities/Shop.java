c3a211cee5cec428a349d7b0921d0371fa04f56f
douniahm <36522492+douniahm@users.noreply.github.com>
2019-04-15 11:57:39 +0100

    create entities

---
+++ BackEnd/src/main/java/shops/entities/Shop.java
@@ -0,0 +1,79 @@
+package shops.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Shop implements Serializable{
+	@Id @GeneratedValue
+	private Long id;
+	private String nom;
+	private Long distance;
+	private String email;
+	private String tel;
+	 @ManyToMany(mappedBy = "shops",  fetch = FetchType.EAGER ) // cascade = CascadeType.ALL,
+		private Collection<User> users;
+
+	public Shop() {		
+	}
+	public Shop (String nom, Long distance, String email, String tel) {
+		super();
+		this.nom = nom;
+		this.distance = distance;
+		this.email = email;
+		this.tel = tel;
+	}
+	public Shop(Long id, String nom, Long distance, String email, String tel) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.distance = distance;
+		this.email = email;
+		this.tel = tel;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Long getDistance() {
+		return distance;
+	}
+	public void setDistance(Long distance) {
+		this.distance = distance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	@Override
+	public String toString() {
+		return "Shop [id=" + id + ", nom=" + nom + ", distance=" + distance + ", email=" + email + ", tel=" + tel + "]";
+	}
+	/*   @ManyToMany(mappedBy = "shops",  cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Collection<User> users;
+	*/
+}
---
+++ BackEnd/src/main/java/shops/entities/User.java
@@ -0,0 +1,72 @@
+package shops.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class User implements Serializable{
+	@Id @GeneratedValue
+	private Long id;
+	private String login;
+	private String password;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name="users_shops",joinColumns =
+	@JoinColumn(name = "id_user"),
+	inverseJoinColumns = @JoinColumn(name = "id_shop"))
+	private Collection<Shop> shops;
+	public User() {
+		
+	}
+	public User(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+	public User(Long id, String login, String password) {
+		this.id = id;
+		this.login = login;
+		this.password = password;
+	}
+	public User(String status) {}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Collection<Shop> getShops() {
+		return shops;
+	}
+	public void setShops(Collection<Shop> shops) {
+		this.shops = shops;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", password=" + password + "]";
+	}
+	/*@Id @GeneratedValue
+	private Long id;
+	private String login;
+	private String password;
+	@ManyToMany()
+	@JoinTable(name="users_shops",joinColumns =
+	@JoinColumn(name = "id_user"),
+	inverseJoinColumns = @JoinColumn(name = "id_shop"))
+	private Collection<Shop> shops;*/
+
+}
