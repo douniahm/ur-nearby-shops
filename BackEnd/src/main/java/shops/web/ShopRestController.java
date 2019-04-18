0aa429d3599e620c71822b3594733ebaa0f96c76
douniahm <36522492+douniahm@users.noreply.github.com>
2019-04-19 00:37:19 +0100

    refactoring UserRestController and adding ShopRestController

---
+++ BackEnd/src/main/java/shops/web/ShopRestController.java
@@ -0,0 +1,46 @@
+package shops.web;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.CollectionUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import shops.dao.ShopRepository;
+import shops.dao.UserRepository;
+import shops.entities.Shop;
+
+
+@RestController
+@CrossOrigin("*")
+public class ShopRestController {
+	@Autowired //injection
+	private UserRepository userRepo;
+	
+	@RequestMapping(value = "/liked-shops", method = RequestMethod.GET)
+	public Page<Shop> search(@RequestParam(name = "mc", defaultValue = "") String mc,
+			@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "6") int size) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+		//Get user's shop
+		Collection<Shop> shopsList = userRepo.findByLogin(username).getShops();
+		//Filter list of shops
+		CollectionUtils.filter(shopsList, shop -> ((Shop) shop).getNom().contains(mc));
+		//Pagination
+		 Page<Shop> pages = new PageImpl<Shop>((List<Shop>) shopsList);
+		return pages;
+	}
+}
--- BackEnd/src/main/java/shops/web/UserRestController.java
+++ BackEnd/src/main/java/shops/web/UserRestController.java
@@ -1,23 +1,24 @@
 package shops.web;
 
-import java.awt.print.Pageable;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.CollectionUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 
 import shops.dao.ShopRepository;
 import shops.dao.UserRepository;
@@ -33,72 +34,35 @@ public class UserRestController {
 	@Autowired
 	private ShopRepository shopRepo;
 	
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public List<User> getUsers() {
-		return userRepo.findAll();
-	}
-
-	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET) // {id} path variable
-	public User getUser(@PathVariable Long id) {
-		return userRepo.findOne(id);
-	}
-
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public User save(@RequestBody User User) { 
-		return userRepo.save(User);
-	}
-
-	@RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-	public void supprimer(@PathVariable Long id) {
-		userRepo.delete(id);
-	}
-
-	@RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
-	public User update(@PathVariable Long id, @RequestBody User User) {
-		User.setId(id);
-		return userRepo.save(User);
+	@RequestMapping(value = "/like-shop", method = RequestMethod.POST)
+	public ResponseEntity<String> save(@RequestBody Shop shop) {        
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User user = userRepo.findByLogin(username);
+        	if(!user.getShops().contains(shop)) {
+        		Shop added = shopRepo.save(shop);
+                user.getShops().add(added);
+                userRepo.save(user);
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        	}
+        	return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
 	}
 	
-	@GetMapping(produces = "application/json")
-	@RequestMapping({ "/validateLogin" })
-	public User validateLogin() {
-		return new User("User successfully authenticated");
-	}
 	
-	@RequestMapping(value = "/user/shop", method = RequestMethod.POST)
-	public Shop save(@RequestBody Shop shop) {
-		Shop addedShop = shopRepo.save(shop); 
-		User user1 = userRepo.getOne((long)1);
-		user1.getShops().add(addedShop);
-		userRepo.save(user1);
-		return addedShop;
+	@RequestMapping(value = "/unlike-shop/{id}", method = RequestMethod.DELETE)
+	public  ResponseEntity<String> delete(@PathVariable Long id) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User user = userRepo.findByLogin(username);
+        Shop shop = shopRepo.findOne(id);
+        	if(user.getShops().contains(shop)) {
+                user.getShops().remove(shop);
+                userRepo.save(user);//update user
+        		shopRepo.delete(shop);//delete shop from db
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        	}
+        	return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-	
-	@RequestMapping(value = "/user/shops", method = RequestMethod.GET)
-	public Page<Shop> search(@RequestParam(name = "mc", defaultValue = "") String mc,
-			@RequestParam(name = "page", defaultValue = "0") int page,
-			@RequestParam(name = "size", defaultValue = "6") int size) {
-		//Get user's shop
-		Collection<Shop> shopsList = userRepo.getOne((long)1).getShops();
-		//Filter list of shops
-		CollectionUtils.filter(shopsList, shop -> ((Shop) shop).getNom().contains(mc));
-		//Pagination
-		 Page<Shop> pages = new PageImpl<Shop>((List<Shop>) shopsList);
-		return pages;
-	}
-	
-	@RequestMapping(value = "/user/shops/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable Long id) {
-		User user1 = userRepo.getOne((long)1);
-		Shop shop = shopRepo.getOne(id);
-		//delete shop from user's likedshops
-		user1.getShops().remove(shop);
-		//update user
-		userRepo.save(user1);
-		//delete shop
-		shopRepo.delete(id);
-	}
-
 }
 
  
