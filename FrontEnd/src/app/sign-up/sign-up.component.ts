e5821329df445f144bcc82baec241cf0ec11ae9e
douniahm <36522492+douniahm@users.noreply.github.com>
2019-04-20 00:23:39 +0100

    configure  sign up service and component

--- FrontEnd/src/app/sign-up/sign-up.component.ts
+++ FrontEnd/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { SignUpService } from './sign-up.service';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  constructor(private signUpService: SignUpService, private router: Router) { }
 
-  ngOnInit() {
+  signUpform = new FormGroup({
+    login : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password : new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  onSignUp(){
+    if(this.signUpform.valid)
+      this.signUpService.signup(
+        new User(this.signUpform.get('login').value, this.signUpform.get('password').value)
+        ).subscribe(
+          res => res,
+          err => {
+            console.log(err);
+          }
+        );
+
   }
 
+  onSignIn(){
+    this.router.navigate(['/login'])
+  }
 }
--- FrontEnd/src/app/sign-up/sign-up.service.ts
+++ FrontEnd/src/app/sign-up/sign-up.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  signup(user: User){
+    return this.http.post("http://localhost:8088/sign-up",user, {observe: 'response'})
+    .pipe(
+      map(
+        res=> {
+          this.router.navigate(["/login"])
+        },
+        err => {
+          console.log(err);
+        }
+      )
+    );
+  }
 }
